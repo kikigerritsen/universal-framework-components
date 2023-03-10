e64f94c5eb3240ae1572da76aaf60e83f750b03e
Kiki Gerritsen <kiki@Kikis-MacBook-Pro.local>
2023-03-10 12:48:00 +0100

    Cleanup

--- packages/test-prj/src/components/App/App.tsx
+++ packages/test-prj/src/components/App/App.tsx
@@ -5,7 +5,6 @@ import { css } from "@emotion/react";
 
 import "./App.css";
 
-import logo from '../../assets/logo.svg';
 import Button from "../Button";
 import { ButtonColor, ButtonVariant } from "shared-styles";
 
@@ -20,26 +19,20 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header" style={{color: "#ffffff"}}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div css={Kaas}>This is a darkorchid Kaas.</div>
-        <Button color={ButtonColor.PRIMARY} iconAfter="forward3">Button icon right</Button>
-        <Button color={ButtonColor.PRIMARY} variant={ButtonVariant.OUTLINE} iconBefore="backward2">Button icon left</Button>
-        <Button color={ButtonColor.SECONDARY} iconBefore="backward2">Button icon left</Button>
-        <Button color={ButtonColor.SECONDARY} variant={ButtonVariant.OUTLINE} iconBefore="backward2">Button icon left</Button>
-        <Button color={ButtonColor.TERTIARY} iconBefore="backward2">Button icon left</Button>
-        <Button color={ButtonColor.TERTIARY} variant={ButtonVariant.OUTLINE} iconBefore="backward2">Button icon left</Button>
+      <header className="App-header">
+        <div css={Kaas}>Custom styling</div>
+        <div style={{color: "#ffffff", display: "flex", alignItems: "center", flexDirection: "column", flexWrap: "wrap"}}>
+          <div>
+            To test the "inherit" on the button text color,<br/>
+            the style of the parent element is <code>`color: "#ffffff";`</code>
+          </div>
+          <Button color={ButtonColor.PRIMARY} iconAfter="forward3">Button icon right</Button>
+          <Button color={ButtonColor.PRIMARY} variant={ButtonVariant.OUTLINE} iconBefore="backward2">Button icon left</Button>
+          <Button color={ButtonColor.SECONDARY} iconBefore="backward2">Button icon left</Button>
+          <Button color={ButtonColor.SECONDARY} variant={ButtonVariant.OUTLINE} iconBefore="backward2">Button icon left</Button>
+          <Button color={ButtonColor.TERTIARY} iconBefore="backward2">Button icon left</Button>
+          <Button color={ButtonColor.TERTIARY} variant={ButtonVariant.OUTLINE} iconBefore="backward2">Button icon left</Button>
+        </div>
       </header>
     </div>
   );
